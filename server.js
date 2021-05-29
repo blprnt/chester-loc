bd560f0defb029e9d3199945b0b46175cb403dc9
Jer Thorp <blprnt@blprnt.com>
2021-05-29 08:40:40 -0400

    Adding all files.

--- server.js
+++ server.js
@@ -9,6 +9,6 @@ var app = express();
 app.use(express.static('views'));
 
 // listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+var listener = app.listen(5676, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
